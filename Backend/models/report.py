b7f563a002616f2dc456ae4f7dcba5ae69bbf6ec
Chinwike Nwokeabia <collins.2007.nc@gmail.com>
2026-04-29 18:42:34 -0400

    added to room and report

--- Backend/models/report.py
+++ Backend/models/report.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String
-from session import Base
+from sqlalchemy import Column, DateTime, Integer, String, func, ForeignKey
+from database.session import Base
 
 class Report(Base):
     __tablename__ = 'reports'
@@ -7,7 +7,6 @@ class Report(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String)
     description = Column(String)
-
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
+    building_id = Column(Integer, ForeignKey = 'buildings.id')
+    time = Column(DateTime,server_default=func.now())
+    status = Column(String, default="pending")
---
+++ Backend/models/room.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database.session import Base
+
+class Room(Base):
+    __tablename__ = 'rooms'
+
+
+    id = Column(Integer, primary_key=True)
+    building_id = Column(Integer, foreignKey = 'buildings.id')
+    room_name = Column(String)
+    floor= Column(Integer, default=1)
+    
