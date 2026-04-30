bc737f6ea39f60a943295988015153a6541b5bb8
Obawole Adesemowo Samuel <adese@sammy.localdomain>
2026-04-30 19:39:52 -0400

    Added Building Schema to Backend

---
+++ Backend/schemas/building.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class BuildingBase(BaseModel):
+    name: str
+    description: Optional[str] = ""
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+    open_time: Optional[str] = "08:00"
+    close_time: Optional[str] = "22:00"
+    facilities: Optional[List[str]] = []
+
+class BuildingCreate(BuildingBase):
+    pass
+
+class BuildingOut(BuildingBase):
+    name: str
+    description: str
+    open_time: str
+    close_time: str
+    facilities: List[str]
+
+    class Config:
+        from_attributes = True
