dbe09cfc13ae3553b093f214e0c66954a88d34b4
Chinwike Nwokeabia <collins.2007.nc@gmail.com>
2026-04-30 21:25:42 -0400

    Modified session.py and reports router

--- Backend/routers/reports.py
+++ Backend/routers/reports.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.session import SessionLocal
 from models.report import Report
+from database.session import get_db
 from schemas.report import ReportCreate, ReportResponse, ReportUpdate
 from auth import verify_admin
 from fastapi import Header, HTTPException, status
@@ -9,14 +10,6 @@ from fastapi import Header, HTTPException, status
 
 
 router = APIRouter()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 @router.post("/reports/", response_model=ReportResponse)
 def create_report(report: ReportCreate, db: Session = Depends(get_db)):
