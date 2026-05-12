2d68be0d123468e8396cc0a7039709b11ca2f482
Obawole Adesemowo Samuel <adese@sammy.localdomain>
2026-05-12 17:35:40 -0400

    made a Homepage.css file to design how the map page should look like in the HomePage.tsx file

--- Frontend/src/Components/HomePage.tsx
+++ Frontend/src/Components/HomePage.tsx
@@ -1,22 +1,28 @@
 import {MapContainer, TileLayer, Marker, Popup } from 'react-leaflet' //importing react components
 import 'leaflet/dist/leaflet.css'
+import './HomePage.css'
 
 
 const CAMPUS_CENTER: [number, number] = [42.3034, -83.0660]
 
 export default function MapView() {
   return (
-   <div style={{ height: '100vh', width: '100vw', background: 'green' }}>
-      <MapContainer
-        center={CAMPUS_CENTER}
-        zoom={17}
-        style={{ height: '100vh', width: '100vw' }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+    <div className = "home-layout">
+      <header className = "header">
+        <h1 className = "Project">Pathfinder</h1>
+      </header>
+       <div className="map-wrapper">
+        <MapContainer
+          center={CAMPUS_CENTER}
+          zoom={17}
+          style={{ height: '100vh', width: '100vw' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
     </div>
   )
 }
