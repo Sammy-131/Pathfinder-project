b6a2c97cf920e346bb48756dd469a4a7c3843c35
Chinwike Nwokeabia <collins.2007.nc@gmail.com>
2026-05-12 18:03:40 -0400

    added settings and report button

--- Frontend/src/Components/HomePage.tsx
+++ Frontend/src/Components/HomePage.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react'
 import {MapContainer, TileLayer, Marker, Popup } from 'react-leaflet' //importing react components
 import 'leaflet/dist/leaflet.css'
 import './HomePage.css'
+import { Link } from 'react-router-dom'
 
 
 const CAMPUS_CENTER: [number, number] = [42.3034, -83.0660]
 
+
 export default function MapView() {
+  const [showSettings, setShowSettings] = useState(false)
   return (
     <div className = "home-layout">
       <header className = "header">
         <h1 className = "Project">Pathfinder</h1>
       </header>
+
+    <div className="toolbar">
+        <button className="settings-btn" onClick={() => setShowSettings(true)}>
+          ⚙ Settings
+        </button>
+      </div>
+
+
+
        <div className="map-wrapper">
         <MapContainer
           center={CAMPUS_CENTER}
@@ -23,6 +36,23 @@ export default function MapView() {
           />
         </MapContainer>
       </div>
+      
+      {showSettings && (
+        <div className="modal-overlay" onClick={() => setShowSettings(false)}>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-header">
+              <h2>Settings</h2>
+              <button className="modal-close" onClick={() => setShowSettings(false)}>✕</button>
+            </div>
+            <div className="modal-body">
+              <Link to="/report" className="settings-option">
+               Report
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+      
     </div>
   )
 }
