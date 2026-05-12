9193c82077fb43410a0ac44439482a32f4c4719d
Obawole Adesemowo Samuel <adese@sammy.localdomain>
2026-05-12 00:23:37 -0400

    made the App.css file and modified the App.tsx file

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ReportPage from './Components/ReportPage'
+import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+    <div className = "app-layout"> 
+      <header className = "top-bar">
+        <h1>Campus Map</h1>
+      </header>
+      <main className = "map-area"> 
+        <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/report" element={<ReportPage />} />
       </Routes>
+      </main>
+    </div>
     </BrowserRouter>
   )
 }
